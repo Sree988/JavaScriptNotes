f94496db9dc4ad0c1af7fbc41ed9efce1cfd7761
Srijani988 <srijani.das988@gmail.com>
2022-08-28 23:32:57 +0530

    first commit

---
+++ 05_Chapter1_PS.js
@@ -0,0 +1,30 @@
+// Chapter 1 - Q1
+let a = "sree"
+let b = 6
+console.log(a + b)
+
+// Chapter 1 - Q2
+console.log(typeof (a+b))
+
+// Chapter 1 - Q3
+const a1 = {
+  name: "sree",
+  section: 1,
+  isPrincipal: false
+}
+// a1 = 45
+// a1 = {}
+
+// Chapter 1 - Q4
+a1['friend'] = "Shubha"  
+a1['name'] = "Satish"  
+console.log(a1)
+
+// Chapter 1 - Q5
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
